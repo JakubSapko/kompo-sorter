f38380cb94b04184dc8953a70bebe83545459198
JakubSapko <jakub.sapko1@gmail.com>
2022-12-03 14:52:14 +0100

    add: basic positioning and sizing, main frame

--- gui/MainApplication.py
+++ gui/MainApplication.py
@@ -1,16 +1,22 @@
 import tkinter as tk
 from typing import NoReturn
 
+from components.Main import Main
+
 class MainApplication(tk.Frame):
     def __init__(self, parent, *args, **kwargs):
         tk.Frame.__init__(self, parent, *args, **kwargs)
         self.parent = parent
-        self.setup_widgets()
 
-    def setup_widgets(self) -> NoReturn:
+        self.main = Main(self, bg="red")
+        self.main.pack(side="right", fill="both", expand=True)
+        self._setup_size_and_positioning()
+
+    def _setup_size_and_positioning(self) -> NoReturn:
         self.winfo_toplevel().title('File sorter')
         self.winfo_toplevel().geometry("800x600+300+200")
 
+        
 
 if __name__ == "__main__":
     root = tk.Tk()
