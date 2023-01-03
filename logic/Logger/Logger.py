774fc0990b6cf307ef226e8f8e40d8c8d655c0e1
JakubSapko <jakub.sapko1@gmail.com>
2023-01-03 17:45:58 +0100

    add: basic logger setup

--- logic/Logger/Logger.py
+++ logic/Logger/Logger.py
@@ -1,4 +1,4 @@
-
+import os
 
 
 class Singleton(type):
@@ -9,3 +9,15 @@ class Singleton(type):
             cls._instances[cls] = instance
         return cls._instances[cls]
 
+class Logger(metaclass=Singleton):
+
+    def __init__(self, logfile: str = None, exception_file: str = "errors") -> None:
+        self._logfile = self.create_logfile(logfile) if logfile else None
+        self._exceptions_file = self.create_logfile(exception_file)
+
+    def create_logfile(self, logfile_name: str) -> str:
+        if not os.path.isdir("./logs"):
+            os.mkdir("./logs")
+        logfile_path: str = f"./logs/{logfile_name}.txt"
+        open(logfile_path, 'a').close()
+        return logfile_path
